e75c0bebe80c0340ae4c479269f799504077899f
brian604 <kjwk1221@gmail.com>
2025-02-18 10:37:01 +0900

    feat: expose QueryBuilder in package __init__

--- medrxiv_langchain/__init__.py
+++ medrxiv_langchain/__init__.py
@@ -1,4 +1,6 @@
-from .loader import BioRxivLoader
+"""MedRxiv LangChain Loader package."""
+
+from .loader import BioRxivLoader, QueryBuilder
 
 __version__ = "0.1.0"
-__all__ = ["BioRxivLoader"]
+__all__ = ["BioRxivLoader", "QueryBuilder"]
